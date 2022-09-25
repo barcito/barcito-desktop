7fb7197813bd34032b93e2522b1bc4dedb8efe16
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-25 15:42:10 -0300

    ✨ Add not authorized page

---
+++ src/pages/NotAuthorized.jsx
@@ -0,0 +1,17 @@
+import Grid from "@mui/material/Grid";
+import NotAuthorizedImage from "../assets/images/notAuthrorized.png";
+
+function NotAuthorized() {
+  return (
+    <Grid container direction="column" alignItems="center">
+      <Grid item>
+        <img src={NotAuthorizedImage} alt={"not authorized"} />
+      </Grid>
+      <Grid item>
+        <h1>Acceso restringido</h1>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default NotFound;

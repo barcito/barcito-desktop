557cdab2d08d1bcb98e90e58d5b60679c05027f5
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-25 15:41:43 -0300

    🍱 Update not found image to use local image

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
 import Grid from "@mui/material/Grid";
+import NotFoundImage from "../assets/images/notFound.png";
 
 function NotFound() {
   return (
     <Grid container direction="column" alignItems="center">
       <Grid item>
-        <img src="https://i.imgur.com/qIufhof.png" alt={"not found"} />
+        <img src={NotFoundImage} alt={"not found"} />
       </Grid>
       <Grid item>
         <h1>This page could not be found</h1>

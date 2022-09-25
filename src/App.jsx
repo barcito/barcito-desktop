13e4f23719479e54c44c1e6c9c9bf9211c77c074
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-25 15:43:14 -0300

    ✨ Add not authorized page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import MainLayout from "./layout/MainLayout";
 import MinimalLayout from "./layout/MinimalLayout";
 
 const NotFound = Loadable(lazy(() => import("./pages/NotFound")));
+const NotAuthorized = Loadable(lazy(() => import("./pages/NotAuthorized")));
 const DashboardDefault = Loadable(lazy(() => import("./pages/dashboard")));
 const SamplePage = Loadable(lazy(() => import("./pages/demos/SamplePage")));
 const ReactQueryDemo = Loadable(lazy(() => import("./pages/demos/ReactQueryDemo")));

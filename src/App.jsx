a5a6bf22491b5a28f71c50072124bb59ad544a43
Raam4 <ramiro_cardozo22@hotmail.com>
2022-09-26 13:52:02 -0300

    ♻️ routes by role

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,20 @@
-import { lazy, useState } from "react";
-import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
 import ThemeCustomization from "./themes";
 import ScrollTop from "./components/ScrollTop";
 import Loadable from "./components/Loadable";
 import MainLayout from "./layout/MainLayout";
 import MinimalLayout from "./layout/MinimalLayout";
 
+const PublicRoute = Loadable(lazy(() => import("./routes/PublicRoute")));
+const PrivateRoute = Loadable(lazy(() => import("./routes/PrivateRoute")));
 const NotFound = Loadable(lazy(() => import("./pages/NotFound")));
 const DashboardDefault = Loadable(lazy(() => import("./pages/dashboard")));
 const SamplePage = Loadable(lazy(() => import("./pages/demos/SamplePage")));
-const ReactQueryDemo = Loadable(
-  lazy(() => import("./pages/demos/ReactQueryDemo"))
-);
+const ReactQueryDemo = Loadable(lazy(() => import("./pages/demos/ReactQueryDemo")));
 const AuthLogin = Loadable(lazy(() => import("./pages/authentication/Login")));
-const AuthRegister = Loadable(
-  lazy(() => import("./pages/authentication/Register"))
-);
+const AuthRegister = Loadable(lazy(() => import("./pages/authentication/Register")));
 const Testing = Loadable(lazy(() => import("./pages/dashboard/testing")));
-
-const ProtectedRoute = () => {
-  const [logged, setLogged] = useState(
-    localStorage.getItem("email") != null ? true : false
-  );
-
-  return logged ? <ProtectedAdminRoute /> : <Navigate to="/login" />;
-};
-
-const ProtectedAdminRoute = () => {
-  const [manager, setManager] = useState(localStorage.getItem("roles"));
-  let permission = true;
-
-  if (
-    !manager.includes("admin") &&
-    !manager.includes("manager") &&
-    !manager.includes("submanager")
-  )
-    permission = false;
-
-  return permission ? <Outlet /> : <NotFound replace={true} />;
-};
-
-const LoggedProtectedRoute = () => {
-  const [logged, setLogged] = useState(
-    localStorage.getItem("email") != null ? true : false
-  );
-
-  return logged ? <Navigate to="/" /> : <Outlet />;
-};
 
 function App() {
   return (
@@ -54,7 +22,7 @@ function App() {
       <ScrollTop>
         <Routes>
           <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<ProtectedRoute />}>
+          <Route path="/" element={<PrivateRoute role="all" />}>
             <Route path="/" element={<MainLayout />}>
               <Route path="/" element={<DashboardDefault />} />
               <Route path="administracion" element={<DashboardDefault />} />
@@ -65,8 +33,8 @@ function App() {
               <Route path="testing" element={<Testing />} />
             </Route>
           </Route>
-          <Route path="/" element={<LoggedProtectedRoute />}>
-            <Route path="/" element={<MinimalLayout />}>
+          <Route element={<PublicRoute />}>
+            <Route element={<MinimalLayout />}>
               <Route path="login" element={<AuthLogin />} />
               <Route path="register" element={<AuthRegister />} />
             </Route>
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+const PrivateRoute = ({ children, role }) => {
+    if(localStorage.getItem("email") != null){
+        if(localStorage.getItem("roles").includes(role) || role === "all"){
+            return children ? children : <Outlet />;
+        }
+        return <Navigate to="/" replace />
+    }
+    return <Navigate to="/login" />;
+};
+
+export default PrivateRoute;

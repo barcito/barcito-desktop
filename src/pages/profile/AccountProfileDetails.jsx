b51cf52053457d5e7a895c6eb9873d86f73015cf
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-09-27 13:56:27 -0300

    💬 Update form texts

--- src/pages/profile/AccountProfileDetails.jsx
+++ src/pages/profile/AccountProfileDetails.jsx
@@ -1,14 +1,5 @@
 import { useState } from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  Grid,
-  TextField,
-} from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, Divider, Grid, TextField } from "@mui/material";
 
 const states = [
   {
@@ -45,78 +36,27 @@ export const AccountProfileDetails = (props) => {
   return (
     <form autoComplete="off" noValidate {...props}>
       <Card>
-        <CardHeader subheader="The information can be edited" title="Profile" />
+        <CardHeader subheader="Ésta información se puede editar" title="Perfil" />
         <Divider />
         <CardContent>
           <Grid container spacing={3}>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                helperText="Please specify the first name"
-                label="First name"
-                name="firstName"
-                onChange={handleChange}
-                required
-                value={values.firstName}
-                variant="outlined"
-              />
+              <TextField fullWidth helperText="Ingrese su nombre" label="Nombre" name="name" onChange={handleChange} required value={values.firstName} variant="outlined" />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Last name"
-                name="lastName"
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                variant="outlined"
-              />
+              <TextField fullWidth label="Apellido" name="apellido" onChange={handleChange} required value={values.lastName} variant="outlined" />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Email Address"
-                name="email"
-                onChange={handleChange}
-                required
-                value={values.email}
-                variant="outlined"
-              />
+              <TextField fullWidth label="Email" name="email" onChange={handleChange} required value={values.email} variant="outlined" />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Phone Number"
-                name="phone"
-                onChange={handleChange}
-                type="number"
-                value={values.phone}
-                variant="outlined"
-              />
+              <TextField fullWidth label="Teléfono" name="telefono" onChange={handleChange} type="number" value={values.phone} variant="outlined" />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Country"
-                name="country"
-                onChange={handleChange}
-                required
-                value={values.country}
-                variant="outlined"
-              />
+              <TextField fullWidth label="Country" name="country" onChange={handleChange} required value={values.country} variant="outlined" />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField
-                fullWidth
-                label="Select State"
-                name="state"
-                onChange={handleChange}
-                required
-                select
-                SelectProps={{ native: true }}
-                value={values.state}
-                variant="outlined"
-              >
+              <TextField fullWidth label="Select State" name="state" onChange={handleChange} required select SelectProps={{ native: true }} value={values.state} variant="outlined">
                 {states.map((option) => (
                   <option key={option.value} value={option.value}>
                     {option.label}
@@ -135,7 +75,7 @@ export const AccountProfileDetails = (props) => {
           }}
         >
           <Button color="primary" variant="contained">
-            Save details
+            Editar
           </Button>
         </Box>
       </Card>

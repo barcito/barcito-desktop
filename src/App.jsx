493528f5032f79252027cec8cc7e55586cb6922d
Raam4 <ramiro_cardozo22@hotmail.com>
2022-09-29 20:34:41 -0300

    new page - barcitos

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const SamplePage = Loadable(lazy(() => import("./pages/demos/SamplePage")));
 const ReactQueryDemo = Loadable(lazy(() => import("./pages/demos/ReactQueryDemo")));
 const AuthLogin = Loadable(lazy(() => import("./pages/authentication/Login")));
 const AuthRegister = Loadable(lazy(() => import("./pages/authentication/Register")));
+const Barcitos = Loadable(lazy(() => import("./pages/admin/barcitos")));
 const Users = Loadable(lazy(() => import("./pages/admin/users")));
 const Account = Loadable(lazy(() => import("./pages/profile/Account")));
 
@@ -28,7 +29,7 @@ function App() {
             <Route path="/" element={<MainLayout />}>
               <Route path="/" element={<DashboardDefault />} />
               <Route path="administracion" element={<DashboardDefault />} />
-              <Route path="barcitos" element={<DashboardDefault />} />
+              <Route path="barcitos" element={<Barcitos />} />
               <Route path="usuarios" element={<Users />} />
               <Route path="sample-page" element={<SamplePage />} />
               <Route path="perfil" element={<Account />} />

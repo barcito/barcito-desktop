9cf7ca209c462a9f5495499b879dc2bf5dc8f717
Raam4 <ramiro_cardozo22@hotmail.com>
2022-09-29 20:07:50 -0300

    roles and validation

--- src/pages/admin/users/UserEditModal.jsx
+++ src/pages/admin/users/UserEditModal.jsx
@@ -1,88 +1,76 @@
-import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
 import FormHelperText from "@mui/material/FormHelperText";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
 import InputLabel from "@mui/material/InputLabel";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
 import * as Yup from "yup";
 import { Formik } from "formik";
 import AnimateButton from "../../../components/AnimateButton";
-import { strengthColor, strengthIndicator } from "../../../utils/password.util";
-import { EyeOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
 import { UserAPI } from "../../../services/userAPI";
-import { MenuItem, Select } from "@mui/material";
+import { MenuItem, Select, FormGroup, Checkbox, FormControlLabel, FormControl } from "@mui/material";
 
 export default function UserEditModal({ user, modalOpen, closeModal, setEditUser }) {
 
     const academicUnits = [
         "Sin definir",
         "Facultad de Informatica"
-      ];
-    
-      const [level, setLevel] = useState();
-      const [showPassword, setShowPassword] = useState(false);
-      const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-      };
-    
-      const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
-    
-      const changePassword = (value) => {
-        const temp = strengthIndicator(value);
-        setLevel(strengthColor(temp));
-      };
-    
-      useEffect(() => {
-        changePassword("");
-      }, []);
+    ];
+
+    const availableRoles = [
+        "admin",
+        "manager",
+        "submanager",
+        "user"
+    ]
+
+    const initialValues = {
+        name: "" || user?.fullName.split(" ")[1],
+        surname: "" || user?.fullName.split(" ")[0],
+        email: "" || user?.email,
+        /* password: "", */
+        academicUnit: "Sin definir" || user?.academicUnit,/* 
+        certificate: "" || user?.certificate, */
+        phone: "" || user?.phone,
+        dni: "" || user?.dni, 
+        roles: user?.roles || [],
+    }
+
+    const asArray = Object.entries(initialValues);
 
     return (
         <Dialog open={modalOpen} onClose={() => closeModal(false)}>
             <DialogTitle>Editar usuario</DialogTitle>
             <DialogContent>
                 <Formik
-                    initialValues={{
-                        name: "" || user?.fullName.split(" ")[1],
-                        surname: "" || user?.fullName.split(" ")[0],
-                        email: "" || user?.email,
-                        /* password: "", */
-                        academicUnit: "Sin definir" || user?.academicUnit,/* 
-                        certificate: "" || user?.certificate, */
-                        phone: "" || user?.phone,
-                        dni: "" || user?.dni,/* 
-                        roles: [] || user?.roles, */
-                    }}
+                    initialValues={initialValues}
                     validationSchema={Yup.object().shape({
                         name: Yup.string().max(255).required("Campo obligatorio"),
                         surname: Yup.string().max(255).required("Campo obligatorio"),
                         email: Yup.string().email("El email es inválido").max(255).required("Campo obligatorio"),
-                        /* password: Yup.string().max(255).required("Campo obligatorio"), */
-                        phone: Yup.number().required("Campo obligatorio"),
-                        dni: Yup.number().required("Campo obligatorio"),
+                        phone: Yup.number().min(100000000, "Teléfono inválido").max(99999999999, "Teléfono inválido").required("Campo obligatorio"),
+                        dni: Yup.number().min(1000000, "DNI inválido").max(99999999, "DNI inválido").required("Campo obligatorio"),
+                        roles: Yup.array().min(1, 'Seleccionar al menos un rol')
                     })}
                     onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
                         try {
                             let response = null;
                             if(user?.id){
-                                response = await UserAPI.update(user.id, values, true);
+                                const filtered = Object.entries(values).filter( (field, i) => 
+                                    field[1] !== asArray[i][1]
+                                );
+                                const dataToSend = Object.fromEntries(filtered);
+                                response = await UserAPI.update(user.id, dataToSend, true);
                             }else{
                                 const data = {
                                     ...values,
                                     password: "123456"
                                 }
-                                response = await UserAPI.create(values, true);
+                                response = await UserAPI.create(data, true);
                             }
                             if (response) {
                                 setStatus({ success: true });
@@ -133,61 +121,10 @@ export default function UserEditModal({ user, modalOpen, closeModal, setEditUser
                                         )}
                                     </Stack>
                                 </Grid>
-                                {/* <Grid item xs={12}>
-                                    <Stack spacing={1}>
-                                        <InputLabel htmlFor="password-edit">Contraseña</InputLabel>
-                                        <OutlinedInput
-                                            fullWidth
-                                            error={Boolean(touched.password && errors.password)}
-                                            id="password-edit"
-                                            type={showPassword ? "text" : "password"}
-                                            value={values.password}
-                                            name="password"
-                                            onBlur={handleBlur}
-                                            onChange={(e) => {
-                                                handleChange(e);
-                                                changePassword(e.target.value);
-                                            }}
-                                            endAdornment={
-                                                <InputAdornment position="end">
-                                                    <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end" size="large">
-                                                        {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                                                    </IconButton>
-                                                </InputAdornment>
-                                            }
-                                            placeholder="******"
-                                            inputProps={{ autoComplete: "current-password" }}
-                                        />
-                                        {touched.password && errors.password && (
-                                            <FormHelperText error id="helper-text-password-edit">
-                                                {errors.password}
-                                            </FormHelperText>
-                                        )}
-                                    </Stack>
-                                    <FormControl fullWidth sx={{ mt: 2 }}>
-                                        <Grid container spacing={2} alignItems="center">
-                                            <Grid item>
-                                                <Box
-                                                    sx={{
-                                                        bgcolor: level?.color,
-                                                        width: 85,
-                                                        height: 8,
-                                                        borderRadius: "7px",
-                                                    }}
-                                                />
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant="subtitle1" fontSize="0.75rem">
-                                                    {level?.label}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </FormControl>
-                                </Grid> */}
                                 <Grid item xs={12}>
                                     <Stack spacing={1}>
                                         <InputLabel htmlFor="phone-edit">Telefono</InputLabel>
-                                        <OutlinedInput fullWidth error={Boolean(touched.phone && errors.phone)} id="phone-edit" value={values.phone} name="phone" onBlur={handleBlur} onChange={handleChange} placeholder="12.345.678" inputProps={{}} />
+                                        <OutlinedInput fullWidth error={Boolean(touched.phone && errors.phone)} id="phone-edit" value={values.phone} name="phone" onBlur={handleBlur} onChange={handleChange} placeholder="12.345.678" inputProps={{}} type="number" />
                                         {touched.phone && errors.phone && (
                                             <FormHelperText error id="helper-text-phone-edit">
                                                 {errors.phone}
@@ -198,7 +135,7 @@ export default function UserEditModal({ user, modalOpen, closeModal, setEditUser
                                 <Grid item xs={12}>
                                     <Stack spacing={1}>
                                         <InputLabel htmlFor="dni-edit">DNI</InputLabel>
-                                        <OutlinedInput fullWidth error={Boolean(touched.dni && errors.dni)} id="dni-edit" value={values.dni} name="dni" onBlur={handleBlur} onChange={handleChange} placeholder="12.345.678" inputProps={{}} />
+                                        <OutlinedInput fullWidth error={Boolean(touched.dni && errors.dni)} id="dni-edit" value={values.dni} name="dni" onBlur={handleBlur} onChange={handleChange} placeholder="12.345.678" inputProps={{}} type="number" />
                                         {touched.dni && errors.dni && (
                                             <FormHelperText error id="helper-text-dni-edit">
                                                 {errors.dni}
@@ -218,7 +155,33 @@ export default function UserEditModal({ user, modalOpen, closeModal, setEditUser
                                     </Stack>
                                 </Grid>
 
-                                
+                                <Grid item xs={12}>
+                                    <Stack spacing={1}>
+                                        <InputLabel htmlFor="roles-edit">Roles</InputLabel>
+                                            <FormControl component="fieldset" variant="standard" error={Boolean(touched.roles && errors.roles)} onBlur={handleBlur} onChange={handleChange}>
+                                                <FormGroup>
+                                                    <Grid container>
+                                                        {availableRoles.map( (role) =>
+                                                            <Grid item xs={3} key={role}>
+                                                                <FormControlLabel
+                                                                    control={
+                                                                    <Checkbox checked={values.roles.includes(role)} id="roles-edit" name="roles" inputProps={{}} value={role} />
+                                                                    }
+                                                                    label={role}
+                                                                />
+                                                            </Grid>
+                                                        )}
+                                                    </Grid>
+                                                </FormGroup>
+                                            </FormControl>
+                                            {touched.roles && errors.roles && (
+                                                <FormHelperText error id="helper-text-roles-edit">
+                                                    {errors.roles}
+                                                </FormHelperText>
+                                            )}
+                                    </Stack>
+                                </Grid>
+
                                 {errors.submit && (
                                     <Grid item xs={12}>
                                         <FormHelperText error>{errors.submit}</FormHelperText>

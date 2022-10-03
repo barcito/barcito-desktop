f7e13e9f108f2a1d57cf190c7534c5a73dd56f53
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-10-03 19:05:39 -0300

    💬 💄 🎨 icons, translations and conditional text

--- src/pages/admin/users/UserListToolbar.jsx
+++ src/pages/admin/users/UserListToolbar.jsx
@@ -1,25 +1,25 @@
-import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
-import { Toolbar, Tooltip, IconButton, Typography, OutlinedInput, InputAdornment } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import { Toolbar, Tooltip, IconButton, Typography, OutlinedInput, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import DeleteIcon from "@mui/icons-material/Delete";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 const RootStyle = styled(Toolbar)(({ theme }) => ({
   height: 96,
-  display: 'flex',
-  justifyContent: 'space-between',
+  display: "flex",
+  justifyContent: "space-between",
   padding: theme.spacing(0, 1, 0, 3),
 }));
 
 const SearchStyle = styled(OutlinedInput)(({ theme }) => ({
   width: 240,
-  transition: theme.transitions.create(['box-shadow', 'width'], {
+  transition: theme.transitions.create(["box-shadow", "width"], {
     easing: theme.transitions.easing.easeInOut,
     duration: theme.transitions.duration.shorter,
   }),
-  '&.Mui-focused': { width: 320, boxShadow: theme.customShadows.z8 },
-  '& fieldset': {
+  "&.Mui-focused": { width: 320, boxShadow: theme.customShadows.z8 },
+  "& fieldset": {
     borderWidth: `1px !important`,
     borderColor: `${theme.palette.grey[500_32]} !important`,
   },
@@ -36,36 +36,36 @@ export default function UserListToolbar({ numSelected, filterName, onFilterName,
     <RootStyle
       sx={{
         ...(numSelected > 0 && {
-          color: 'primary.main',
-          bgcolor: 'primary.lighter',
+          color: "primary.main",
+          bgcolor: "primary.lighter",
         }),
       }}
     >
       {numSelected > 0 ? (
         <Typography component="div" variant="subtitle1">
-          {numSelected} selected
+          {numSelected} seleccionado
         </Typography>
       ) : (
         <SearchStyle
           value={filterName}
           onChange={onFilterName}
-          placeholder="Search user..."
+          placeholder="Buscar usuario..."
           startAdornment={
             <InputAdornment position="start">
-              <SearchIcon sx={{ color: 'text.disabled', width: 20, height: 20 }} />
+              <SearchIcon sx={{ color: "text.disabled", width: 20, height: 20 }} />
             </InputAdornment>
           }
         />
       )}
 
       {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton onClick={ () => handleDelete() }>
+        <Tooltip title="Borrar Usuario(s)">
+          <IconButton onClick={() => handleDelete()}>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
       ) : (
-        <Tooltip title="Filter list">
+        <Tooltip title="Filtrar Listado">
           <IconButton>
             <FilterListIcon />
           </IconButton>
--- src/pages/admin/users/UserMoreMenu.jsx
+++ src/pages/admin/users/UserMoreMenu.jsx
@@ -1,8 +1,8 @@
-import { useRef, useState } from 'react';
-import { Menu, MenuItem, IconButton, ListItemIcon, ListItemText } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { useRef, useState } from "react";
+import { Menu, MenuItem, IconButton, ListItemIcon, ListItemText } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
+import PersonRemove from "@mui/icons-material/PersonRemove";
 
 export default function UserMoreMenu({ user, handleEdit, handleDelete }) {
   const ref = useRef(null);
@@ -19,23 +19,35 @@ export default function UserMoreMenu({ user, handleEdit, handleDelete }) {
         anchorEl={ref.current}
         onClose={() => setIsOpen(false)}
         PaperProps={{
-          sx: { width: 200, maxWidth: '100%' },
+          sx: { width: 200, maxWidth: "100%" },
         }}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
       >
-        <MenuItem sx={{ color: 'text.secondary' }} onClick={ () => { handleEdit(user); setIsOpen(false); } }>
+        <MenuItem
+          sx={{ color: "text.secondary" }}
+          onClick={() => {
+            handleEdit(user, "Editar ");
+            setIsOpen(false);
+          }}
+        >
           <ListItemIcon>
             <EditIcon width={24} height={24} />
           </ListItemIcon>
-          <ListItemText primary="Edit" primaryTypographyProps={{ variant: 'body2' }} />
+          <ListItemText primary="Editar Usuario" primaryTypographyProps={{ variant: "body2" }} />
         </MenuItem>
 
-        <MenuItem sx={{ color: 'text.secondary' }} onClick={ () => { handleDelete(user); setIsOpen(false); } }>
+        <MenuItem
+          sx={{ color: "text.secondary" }}
+          onClick={() => {
+            handleDelete(user);
+            setIsOpen(false);
+          }}
+        >
           <ListItemIcon>
-            <DeleteIcon width={24} height={24} />
+            <PersonRemove width={24} height={24} />
           </ListItemIcon>
-          <ListItemText primary="Delete" primaryTypographyProps={{ variant: 'body2' }} />
+          <ListItemText primary="Eliminar Usuario" primaryTypographyProps={{ variant: "body2" }} />
         </MenuItem>
       </Menu>
     </>

405defcb3d469fcd2fa03cef46b65c14b0f48153
Raam4 <ramiro_cardozo22@hotmail.com>
2022-10-26 18:42:31 -0300

    status update

--- src/pages/manager/orders/OrderDetails.jsx
+++ src/pages/manager/orders/OrderDetails.jsx
@@ -1,14 +1,22 @@
-import { Container, Divider, Grid, Stack, Typography, Card, Box, CardMedia, CardContent } from "@mui/material";
+import { Container, Divider, Grid, Stack, Typography, Card, CardMedia, CardContent, Button, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { OrdersAPI } from "@/services/ordersAPI";
 import MainCard from "@/components/MainCard";
 import OrderStatus from "@/components/order-list-table/OrderStatus";
+import { useState } from "react";
 
 export default function OrderDetails() {
     const params = useParams();
-
     const { data: order, isLoading } = useQuery(['order'], () => OrdersAPI.getByCode(params.orderCode));
+    const [status, setStatus] = useState("");
+    const statusOptions = ['Rechazado', 'Cancelado', 'Pendiente', 'Preparado', 'Entregado'];
+
+    const handleSave = () => {
+        if( status !== "" ){
+            OrdersAPI.update(order.id, {status: status});
+        }
+    }
 
     if (isLoading) {
         return <p>Loading...</p>;
@@ -61,44 +69,69 @@ export default function OrderDetails() {
                             Productos
                         </Typography>
                     </Grid>
-                {order.products.map((prod) => (
-                    <Grid item xs={12} key={prod.id}>
-                        <Card variant="outlined" sx={{ display: 'flex' }}>
-                            <CardMedia
-                                component="img"
-                                sx={{ width: 151 }}
-                                image={prod.product.imagePath}
-                                alt={prod.product.description}
-                            />
-                            <CardContent sx={{ width: '100%'}}>
-                                <Stack direction="row" spacing={3} justifyContent="space-around" alignItems="center">
-                                    <Typography component="div" variant="h5">
-                                        {`${prod.product.description} x${prod.quantity}`}
-                                    </Typography>
-                                    <Stack>
-                                        <Typography component="div" variant="subtitle1">
-                                            Precio unitario: 
+                    {order.products.map((prod) => (
+                        <Grid item xs={12} key={prod.id}>
+                            <Card variant="outlined" sx={{ display: 'flex' }}>
+                                <CardMedia
+                                    component="img"
+                                    sx={{ width: 151 }}
+                                    image={prod.product.imagePath}
+                                    alt={prod.product.description}
+                                />
+                                <CardContent sx={{ width: '100%' }}>
+                                    <Stack direction="row" spacing={3} justifyContent="space-around" alignItems="center">
+                                        <Typography component="div" variant="h5">
+                                            {`${prod.product.description} x${prod.quantity}`}
                                         </Typography>
-                                        <Typography component="div" variant="h4" >
-                                            ${prod.lockedPrice}
-                                        </Typography>
+                                        <Stack>
+                                            <Typography component="div" variant="subtitle1">
+                                                Precio unitario:
+                                            </Typography>
+                                            <Typography component="div" variant="h4" >
+                                                ${prod.lockedPrice}
+                                            </Typography>
+                                        </Stack>
+                                        <Stack>
+                                            <Typography component="div" variant="subtitle1">
+                                                Precio total:
+                                            </Typography>
+                                            <Typography component="div" variant="h4" >
+                                                ${prod.lockedPrice * prod.quantity}
+                                            </Typography>
+                                        </Stack>
                                     </Stack>
-                                    <Stack>
-                                    <Typography component="div" variant="subtitle1">
-                                            Precio total: 
-                                        </Typography>
-                                        <Typography component="div" variant="h4" >
-                                            ${prod.lockedPrice * prod.quantity}
-                                        </Typography>
-                                    </Stack>
-                                </Stack>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
                 </Grid>
                 <Divider sx={{ my: 3 }} />
-                Acciones
+                <Grid container spacing={1} alignItems="center">
+                    <Grid item xs={12}>
+                        <Typography component="div" variant="h5">
+                            Modificar estado:
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <FormControl fullWidth>
+                            <InputLabel id="status-select-label">Estado</InputLabel>
+                            <Select
+                                labelId="status-select-label"
+                                id="status-select"
+                                label="Estado"
+                                value={status}
+                                onChange={(e)=>setStatus(e.target.value)}
+                            >
+                                {statusOptions.map( (option) => 
+                                    <MenuItem key={option} value={option}>{option}</MenuItem>
+                                )}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant="contained" color="primary" onClick={handleSave}>Guardar</Button>
+                    </Grid>
+                </Grid>
             </MainCard>
         </Container>
     );

0c77b8117abdc39564437a5e4d31231ca8a795ed
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-11-14 19:44:31 -0300

    ✨ add category form validation

--- src/pages/manager/categories/CategoryDialog.jsx
+++ src/pages/manager/categories/CategoryDialog.jsx
@@ -1,68 +1,85 @@
-import { forwardRef, useState} from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide, TextField, RadioGroup, FormControlLabel, Radio } from '@mui/material';
-import { useEffect } from 'react';
+import { forwardRef, useState } from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormHelperText, Slide, OutlinedInput, RadioGroup, FormControlLabel, Radio } from "@mui/material";
+import { Formik, useFormikContext, useField, Field, FieldArray } from "formik";
+import * as Yup from "yup";
+import { useEffect } from "react";
 
 const Transition = forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function CategoryDialog({ category, dialogOpen, mutation, setDialogOpen }) {
+  const initialValues = {
+    type: "",
+    description: "",
+  };
 
-    const [description, setDescription] = useState("");
-    const [type, setType] = useState("");
+  return (
+    <Dialog open={dialogOpen} TransitionComponent={Transition} keepMounted onClose={() => setDialogOpen(false)} aria-describedby="alert-dialog-slide-description">
+      <DialogTitle>{category.id ? "Modificar categoría" : "Crear categoría"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">Seleccione el tipo de categor&iacute;a</DialogContentText>
 
-    useEffect(()=>{
-        setDescription(category.description);
-        setType(category.type);
-    }, [category]);
+        <Formik
+          enableReinitialize
+          initialValues={initialValues}
+          validationSchema={Yup.object().shape({
+            type: Yup.mixed().oneOf(["Consumible", "Insumo", "Producto"]).required("Debe seleccionar una categoría"),
+            description: Yup.string().max(255).required("Debe ingresar un nombre para la categoría"),
+          })}
+          onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+            let type = values.type;
+            let description = values.description;
 
-    const handleSubmit = () => {
-        mutation.mutate({id: category.id, description, type});
-        setDialogOpen(false);
-    }
-
-    return (
-        <Dialog
-            open={dialogOpen}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={ () => setDialogOpen(false) }
-            aria-describedby="alert-dialog-slide-description"
+            try {
+              if (category.id) {
+                mutation.mutate({ id: category.id, type, description });
+              } else {
+                mutation.mutate({ type, description });
+              }
+              setStatus({ success: true });
+              setSubmitting(false);
+              setDialogOpen(false);
+            } catch (err) {
+              setStatus({ success: false });
+              setErrors({ submit: err.message });
+              setSubmitting(false);
+            }
+          }}
         >
-            <DialogTitle>{category ? "Modificar categoría" : "Crear categoría"}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    Seleccione el tipo de categor&iacute;a
-                </DialogContentText>
-                <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                    value={type}
-                    onChange={(e) => setType(e.target.value)}
-                >
-                    <FormControlLabel value="Consumible" control={<Radio />} label="Consumible" />
-                    <FormControlLabel value="Insumo" control={<Radio />} label="Insumo" />
-                    <FormControlLabel value="Producto" control={<Radio />} label="Producto" />
-                </RadioGroup>
-                <DialogContentText id="alert-dialog-slide-description" sx={{ mt: 3 }}>
-                    Ingrese el nombre
-                </DialogContentText>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="description"
-                    type="text"
-                    fullWidth
-                    variant="standard"
-                    value={description}
-                    onInput={(e) => setDescription(e.target.value) }
-                />
-            </DialogContent>
-            <DialogActions>
+          {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+            <form noValidate onSubmit={handleSubmit}>
+              <RadioGroup row aria-labelledby="type" name="type" value={values.type} onBlur={handleBlur} onChange={handleChange}>
+                <FormControlLabel value="Consumible" control={<Radio />} label="Consumible" />
+                <FormControlLabel value="Insumo" control={<Radio />} label="Insumo" />
+                <FormControlLabel value="Producto" control={<Radio />} label="Producto" />
+              </RadioGroup>
+              {touched.type && errors.type && (
+                <FormHelperText error id="standard-weight-helper-text-description-item">
+                  {errors.type}
+                </FormHelperText>
+              )}
+
+              <DialogContentText id="alert-dialog-slide-description" sx={{ mt: 3 }}>
+                Ingrese el nombre
+              </DialogContentText>
+              <OutlinedInput id="description" type="text" value={values.description} name="description" onBlur={handleBlur} onChange={handleChange} placeholder="Ingresar descripcion" fullWidth error={Boolean(touched.description && errors.description)} />
+              {touched.description && errors.description && (
+                <FormHelperText error id="standard-weight-helper-text-description-item">
+                  {errors.description}
+                </FormHelperText>
+              )}
+
+              <DialogActions>
                 <Button onClick={() => setDialogOpen(false)}>Cancelar</Button>
-                <Button onClick={handleSubmit}>Aceptar</Button>
-            </DialogActions>
-        </Dialog>
-    );
+                <Button onClick={handleSubmit} disabled={isSubmitting}>
+                  Crear
+                </Button>
+              </DialogActions>
+            </form>
+          )}
+        </Formik>
+      </DialogContent>
+    </Dialog>
+  );
 }
--- src/pages/manager/categories/index.jsx
+++ src/pages/manager/categories/index.jsx
@@ -1,125 +1,132 @@
-import { Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Tooltip, IconButton, Box, Tab } from '@mui/material';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { CategoriesAPI } from '@/services/categoriesAPI';
-import UserMoreMenu from '@/components/user-list-table/UserMoreMenu';
+import { Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Tooltip, IconButton, Box, Tab } from "@mui/material";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { CategoriesAPI } from "@/services/categoriesAPI";
+import UserMoreMenu from "@/components/user-list-table/UserMoreMenu";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import LibraryAdd from "@mui/icons-material/LibraryAdd";
-import CategoryDialog from './CategoryDialog';
-import ConfirmDialog from '@/components/ConfirmDialog';
-import { useState } from 'react';
-import { TabContext, TabList } from '@mui/lab';
+import CategoryDialog from "./CategoryDialog";
+import ConfirmDialog from "@/components/ConfirmDialog";
+import { useState } from "react";
+import { TabContext, TabList } from "@mui/lab";
 
-export default function CategoriesList(){
+export default function CategoriesList() {
+  const client = useQueryClient();
+  const { data: categories, isLoading } = useQuery(["categories"], () => CategoriesAPI.getAll());
 
-    const client = useQueryClient();
-    const {data: categories, isLoading} = useQuery(['categories'], () => CategoriesAPI.getAll());
+  const [categoryDialogOpen, setCategoryDialogOpen] = useState(false);
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [category, setCategory] = useState({ description: "", type: "" });
+  const [value, setValue] = useState("All");
 
-    const [categoryDialogOpen, setCategoryDialogOpen] = useState(false);
-    const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
-    const [category, setCategory] = useState({description: "", type: ""});
-    const [value, setValue] = useState("All");
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const mutation = useMutation(
-        (cat) => {
-            if(cat.id){
-                return CategoriesAPI.update(cat.id, {description: cat.description, type: cat.type});    
-            }
-            return CategoriesAPI.create(cat);
-        },
-        {
-            onSuccess: () => {
-                client.invalidateQueries(['categories']);
-            }
-        }
-    );
-
-    const deleteMutation = useMutation(
-        (id) => {
-            return CategoriesAPI.delete(id);
-        },
-        {
-            onSuccess: () => {
-                setConfirmDialogOpen(false);
-                client.invalidateQueries(['categories']);
-            }
-        }
-    )
-
-    if(isLoading){
-        return <p>Loading...</p>;
+  const mutation = useMutation(
+    (cat) => {
+      if (cat.id) {
+        return CategoriesAPI.update(cat.id, { description: cat.description, type: cat.type });
+      }
+      return CategoriesAPI.create(cat);
+    },
+    {
+      onSuccess: () => {
+        client.invalidateQueries(["categories"]);
+      },
     }
+  );
 
-    const tabbedCategories = value === "All" || !categories ? categories : categories.filter( (category) => category.type === value );
+  const deleteMutation = useMutation(
+    (id) => {
+      return CategoriesAPI.delete(id);
+    },
+    {
+      onSuccess: () => {
+        setConfirmDialogOpen(false);
+        client.invalidateQueries(["categories"]);
+      },
+    }
+  );
 
-    return(
-        <>
-            <Container sx={{pt: 4}}>
-                <TabContext value={value}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange} aria-label="categories tabs">
-                            <Tab label="Todas" value="All" />
-                            <Tab label="Consumibles" value="Consumible" />
-                            <Tab label="Insumos" value="Insumo" />
-                            <Tab label="Productos" value="Producto" />
-                        </TabList>
-                    </Box>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align='center'>Nombre</TableCell>
-                                    <TableCell align='center'>Acciones</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {tabbedCategories.map((row) => (
-                                <TableRow
-                                key={row.id}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <TableCell align='center' component="th" scope="row">
-                                        {row.description}
-                                    </TableCell>
-                                    <TableCell align='center'>
-                                    <UserMoreMenu
-                                        disabled={ false }
-                                        user={row}
-                                        actionOne={{
-                                            label: "Editar",
-                                            icon: <EditIcon width={24} height={24} />,
-                                            fn: () => {setCategory(row); setCategoryDialogOpen(true)}
-                                        }}
-                                        actionTwo={{
-                                            label: "Eliminar",
-                                            icon: <DeleteIcon width={24} height={24} />,
-                                            fn: () => {setCategory(row); setConfirmDialogOpen(true)}
-                                        }}
-                                    />
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                                <TableRow>
-                                    <TableCell colSpan={2} align='center'>
-                                        <Tooltip title="Nueva Categoria">
-                                            <IconButton color="primary" onClick={() => {setCategory({description: "", type: ""}); setCategoryDialogOpen(true)}}>
-                                                <LibraryAdd fontSize="large"/>
-                                            </IconButton>
-                                        </Tooltip>
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </TabContext>
-            </Container>
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-            <CategoryDialog category={category} dialogOpen={categoryDialogOpen} mutation={mutation} setDialogOpen={setCategoryDialogOpen} />
-            <ConfirmDialog dialogOpen={confirmDialogOpen} text={"¿Eliminar categoría?"} confirmAction={() => deleteMutation.mutate(category.id) } closeDialog={setConfirmDialogOpen} />
-        </>
-    );
+  const tabbedCategories = value === "All" || !categories ? categories : categories.filter((category) => category.type === value);
+
+  return (
+    <>
+      <Container sx={{ pt: 4 }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="categories tabs">
+              <Tab label="Todas" value="All" />
+              <Tab label="Consumibles" value="Consumible" />
+              <Tab label="Insumos" value="Insumo" />
+              <Tab label="Productos" value="Producto" />
+            </TabList>
+          </Box>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">Nombre</TableCell>
+                  <TableCell align="center">Acciones</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tabbedCategories.map((row) => (
+                  <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                    <TableCell align="center" component="th" scope="row">
+                      {row.description}
+                    </TableCell>
+                    <TableCell align="center">
+                      <UserMoreMenu
+                        disabled={false}
+                        user={row}
+                        actionOne={{
+                          label: "Editar",
+                          icon: <EditIcon width={24} height={24} />,
+                          fn: () => {
+                            setCategory(row);
+                            setCategoryDialogOpen(true);
+                          },
+                        }}
+                        actionTwo={{
+                          label: "Eliminar",
+                          icon: <DeleteIcon width={24} height={24} />,
+                          fn: () => {
+                            setCategory(row);
+                            setConfirmDialogOpen(true);
+                          },
+                        }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell colSpan={2} align="center">
+                    <Tooltip title="Nueva Categoria">
+                      <IconButton
+                        color="primary"
+                        onClick={() => {
+                          setCategoryDialogOpen(true);
+                        }}
+                      >
+                        <LibraryAdd fontSize="large" />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </TabContext>
+      </Container>
+
+      <CategoryDialog category={category} dialogOpen={categoryDialogOpen} mutation={mutation} setDialogOpen={setCategoryDialogOpen} />
+      <ConfirmDialog dialogOpen={confirmDialogOpen} text={"¿Eliminar categoría?"} confirmAction={() => deleteMutation.mutate(category.id)} closeDialog={setConfirmDialogOpen} />
+    </>
+  );
 }

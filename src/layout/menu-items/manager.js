9e4987e1fc360e16f63323ec3159de98031b8335
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-11-14 21:28:18 -0300

    💄 add menu icons

--- src/layout/menu-items/manager.js
+++ src/layout/menu-items/manager.js
@@ -1,10 +1,20 @@
 import { UserOutlined, BugOutlined, BugFilled, BugTwoTone } from "@ant-design/icons";
+import { FaBoxes, FaReceipt, FaStore, FaUserAlt } from "react-icons/fa";
+import { TbPaperBag } from "react-icons/tb";
+import { SiBuymeacoffee } from "react-icons/si";
+import { MdLabel } from "react-icons/md";
 
 const icons = {
-  UserOutlined,
+  FaUserAlt,
   BugOutlined,
   BugFilled,
-  BugTwoTone
+  BugTwoTone,
+  FaStore,
+  SiBuymeacoffee,
+  TbPaperBag,
+  FaBoxes,
+  FaReceipt,
+  MdLabel,
 };
 
 export const menuManager = {
@@ -17,49 +27,49 @@ export const menuManager = {
       title: "Barcito",
       type: "item",
       url: "/barcito",
-      icon: icons.BugFilled
+      icon: icons.FaStore,
     },
     {
       id: "manager1",
       title: "Pedidos",
       type: "item",
       url: "/pedidos",
-      icon: icons.BugFilled,
+      icon: icons.TbPaperBag,
     },
     {
       id: "manager2",
       title: "Productos",
       type: "item",
       url: "/productos",
-      icon: icons.BugFilled
+      icon: icons.SiBuymeacoffee,
     },
     {
       id: "manager3",
       title: "Stock",
       type: "item",
       url: "/stock",
-      icon: icons.BugFilled
+      icon: icons.FaBoxes,
     },
     {
       id: "manager5",
       title: "Categorías",
       type: "item",
       url: "/categorias",
-      icon: icons.BugFilled
+      icon: icons.MdLabel,
     },
     {
       id: "manager6",
       title: "Recibos",
       type: "item",
       url: "/recibos",
-      icon: icons.BugFilled
+      icon: icons.FaReceipt,
     },
     {
       id: "manager7",
       title: "Socios",
       type: "item",
       url: "/socios",
-      icon: icons.UserOutlined,
+      icon: icons.FaUserAlt,
     },
   ],
 };

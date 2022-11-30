899525136952a7902afbd8c6d90c1f7162345607
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-11-30 18:58:04 -0300

    ✨ add back manager dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -18,41 +18,116 @@ import { OrdersAPI } from "@/services/ordersAPI";
 import { StockAPI } from "@/services/stockAPI";
 import { UserAPI } from "@/services/userAPI";
 
-function formatFetchedData(obj) {
-  for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-      return obj["data"];
-    }
-  }
-}
-
 function DashboardDefault() {
   const [slot, setSlot] = useState("week");
+  let [usersOrdersData, setUsersOrdersData] = useState([]);
+  let [stockData, setStockData] = useState([]);
+  let [usersData, setUsersData] = useState([]);
 
-  const usersOrdersFetch = useQuery(["order"], async () => OrdersAPI.getAll());
-  let usersOrdersData = [];
-
-  const stockFetch = useQuery(["stock"], async () => StockAPI.getAll());
-  let stockData = [];
-
-  const usersFetch = useQuery(["user"], async () => UserAPI.getAll());
-  let usersData = [];
-
-  // Fetched data formatting
-  if (usersOrdersFetch.isSuccess) {
-    usersOrdersData = formatFetchedData(usersOrdersFetch);
-  }
-
-  if (stockFetch.isSuccess) {
-    stockData = formatFetchedData(stockFetch);
-  }
-
-  if (usersFetch.isSuccess) {
-    usersData = formatFetchedData(usersFetch);
-  }
+  let usersOrdersFetch = useQuery(["order"], async () => OrdersAPI.getAll(), { onSuccess: setUsersOrdersData });
+  let stockFetch = useQuery(["stock"], async () => StockAPI.getAll(), { onSuccess: setStockData });
+  let usersFetch = useQuery(["user"], async () => UserAPI.getAll(), { onSuccess: setUsersData });
 
   return (
-    <p>nada</p>
+    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+      {/* row 1 */}
+      <Grid item xs={12} sm={4} md={4} lg={4}>
+        <AssociatedUsersAnalytics usersData={usersData} />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={4}>
+        <DeliveredOrdersAnalytics usersOrdersData={usersOrdersData} />
+      </Grid>
+      <Grid item xs={12} sm={4} md={4} lg={4}>
+        <SalesAnalytics usersOrdersData={usersOrdersData} stockData={stockData} />
+      </Grid>
+      <Grid item md={8} sx={{ display: { sm: "none", md: "block", lg: "none" } }} />
+
+      {/* row 2 */}
+      <Grid item xs={12} md={7} lg={8}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Pedidos Recientes</Typography>
+          </Grid>
+          <Grid item />
+        </Grid>
+        <MainCard sx={{ mt: 2 }} content={false}>
+          <OrdersTable usersOrdersData={usersOrdersData} />
+        </MainCard>
+      </Grid>
+
+      <Grid item xs={12} md={5} lg={4}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Stock</Typography>
+          </Grid>
+          <Grid item />
+        </Grid>
+        <MainCard sx={{ mt: 2 }} content={false}>
+          <StockChart stockData={stockData} />
+        </MainCard>
+        <MainCard sx={{ mt: 2 }}>
+          <Stack spacing={3}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Stack>
+                  <Typography variant="h5" noWrap>
+                    Ayuda y Soporte
+                  </Typography>
+                  <Typography variant="caption" color="secondary" noWrap>
+                    Tiempo de respuesta promedio: 5 minutos
+                  </Typography>
+                </Stack>
+              </Grid>
+            </Grid>
+            <Button size="small" variant="contained" sx={{ textTransform: "capitalize" }}>
+              Enviar Email
+            </Button>
+          </Stack>
+        </MainCard>
+      </Grid>
+
+      {/* row 3 */}
+      <Grid item xs={12} md={5} lg={4}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Cantidad de Ventas</Typography>
+          </Grid>
+          <Grid item />
+        </Grid>
+        <MainCard sx={{ mt: 2 }} content={false}>
+          <Box sx={{ p: 3, pb: 0 }}>
+            <Stack spacing={2}>
+              <Typography variant="h6" color="textSecondary">
+                Estadística Mensual
+              </Typography>
+            </Stack>
+          </Box>
+          <MonthlyBarChart usersOrdersData={usersOrdersData} />
+        </MainCard>
+      </Grid>
+      <Grid item xs={12} md={7} lg={8}>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <Typography variant="h5">Ventas a Usuarios</Typography>
+          </Grid>
+          <Grid item>
+            <Stack direction="row" alignItems="center" spacing={0}>
+              <Button size="small" onClick={() => setSlot("month")} color={slot === "month" ? "primary" : "secondary"} variant={slot === "month" ? "outlined" : "text"}>
+                Mes
+              </Button>
+              <Button size="small" onClick={() => setSlot("week")} color={slot === "week" ? "primary" : "secondary"} variant={slot === "week" ? "outlined" : "text"}>
+                Semana
+              </Button>
+            </Stack>
+          </Grid>
+        </Grid>
+        <MainCard content={false} sx={{ mt: 1.5 }}>
+          <Box sx={{ pt: 1, pr: 2 }}>
+            <IncomeAreaChart slot={slot} usersOrdersData={usersOrdersData} />
+          </Box>
+        </MainCard>
+      </Grid>
+    </Grid>
   );
 }
 

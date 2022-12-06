af154a2c980dd95a019b7972d98775bd52cbded9
jmarcosg <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-12-06 09:05:58 -0300

    ✨ add new categories index

--- src/pages/manager/categories/index.jsx
+++ src/pages/manager/categories/index.jsx
@@ -1,54 +1,37 @@
-import { Container, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Tooltip, IconButton, Box, Tab } from "@mui/material";
+import { Container, Box, Tab } from "@mui/material";
+import { TabContext, TabList } from "@mui/lab";
+import CategoriesList from "@/components/categories-list-table/CategoriesList";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { CategoriesAPI } from "@/services/categoriesAPI";
-import UserMoreMenu from "@/components/user-list-table/UserMoreMenu";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import LibraryAdd from "@mui/icons-material/LibraryAdd";
 import CategoryDialog from "./CategoryDialog";
 import ConfirmDialog from "@/components/ConfirmDialog";
-import { useState } from "react";
-import { TabContext, TabList } from "@mui/lab";
 
-export default function CategoriesList() {
-  const client = useQueryClient();
+const TABLE_HEAD = [
+  { id: "description", label: "Descripcion", alignCenter: false },
+  { id: "type", label: "Tipo", alignCenter: true },
+  { id: "createdAt", label: "Fecha creación", alignCenter: true },
+];
+
+export default function Categories() {
+  const navigate = useNavigate();
+
   const { data: categories, isLoading } = useQuery(["categories"], () => CategoriesAPI.getAll());
 
-  const [categoryDialogOpen, setCategoryDialogOpen] = useState(false);
-  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
-  const [editData, setEditData] = useState({});
-  const [category, setCategory] = useState({ description: "", type: "" });
   const [value, setValue] = useState("All");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const mutation = useMutation(
-    (cat) => {
-      if (cat.id) {
-        return CategoriesAPI.update(cat.id, { description: cat.description, type: cat.type });
-      }
-      return CategoriesAPI.create(cat);
-    },
-    {
-      onSuccess: () => {
-        client.invalidateQueries(["categories"]);
-      },
-    }
-  );
+  const handleNew = () => {
+    navigate(`/categorias/nueva`);
+  };
 
-  const deleteMutation = useMutation(
-    (id) => {
-      return CategoriesAPI.delete(id);
-    },
-    {
-      onSuccess: () => {
-        setConfirmDialogOpen(false);
-        client.invalidateQueries(["categories"]);
-      },
-    }
-  );
+  const handleEdit = (id) => {
+    navigate(`/categorias/editar/${id}`);
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -57,79 +40,18 @@ export default function CategoriesList() {
   const tabbedCategories = value === "All" || !categories ? categories : categories.filter((category) => category.type === value);
 
   return (
-    <>
-      <Container sx={{ pt: 4 }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <TabList onChange={handleChange} aria-label="categories tabs">
-              <Tab label="Todas" value="All" />
-              <Tab label="Consumibles" value="Consumible" />
-              <Tab label="Insumos" value="Insumo" />
-              <Tab label="Productos" value="Producto" />
-            </TabList>
-          </Box>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">Nombre</TableCell>
-                  <TableCell align="center">Acciones</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {tabbedCategories.map((row) => (
-                  <TableRow key={row.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                    <TableCell align="center" component="th" scope="row">
-                      {row.description}
-                    </TableCell>
-                    <TableCell align="center">
-                      <UserMoreMenu
-                        disabled={false}
-                        user={row}
-                        actionOne={{
-                          label: "Editar",
-                          icon: <EditIcon width={24} height={24} />,
-                          fn: () => {
-                            setCategory(row);
-                            setCategoryDialogOpen(true);
-                            setEditData({ ...row });
-                          },
-                        }}
-                        actionTwo={{
-                          label: "Eliminar",
-                          icon: <DeleteIcon width={24} height={24} />,
-                          fn: () => {
-                            setCategory(row);
-                            setConfirmDialogOpen(true);
-                          },
-                        }}
-                      />
-                    </TableCell>
-                  </TableRow>
-                ))}
-                <TableRow>
-                  <TableCell colSpan={2} align="center">
-                    <Tooltip title="Nueva Categoria">
-                      <IconButton
-                        color="primary"
-                        onClick={() => {
-                          setCategoryDialogOpen(true);
-                          setEditData({});
-                        }}
-                      >
-                        <LibraryAdd fontSize="large" />
-                      </IconButton>
-                    </Tooltip>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </TabContext>
-      </Container>
-
-      <CategoryDialog editData={editData} dialogOpen={categoryDialogOpen} mutation={mutation} setDialogOpen={setCategoryDialogOpen} />
-      <ConfirmDialog dialogOpen={confirmDialogOpen} text={"¿Eliminar categoría?"} confirmAction={() => deleteMutation.mutate(category.id)} closeDialog={setConfirmDialogOpen} />
-    </>
+    <Container sx={{ pt: 4 }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="Orders tabs">
+            <Tab label="Todas" value="All" />
+            <Tab label="Consumible" value="Consumible" />
+            <Tab label="Insumo" value="Insumo" />
+            <Tab label="Producto" value="Producto" />
+          </TabList>
+        </Box>
+        <CategoriesList categoriesList={tabbedCategories} tableHead={TABLE_HEAD} handleNew={handleNew} handleEdit={handleEdit} />
+      </TabContext>
+    </Container>
   );
 }
